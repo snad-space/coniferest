4fe9e06a209a708435addd5eda126a7a0e6d2736
Konstantin Malanchev <hombit@gmail.com>
2021-08-11 14:17:06 +0300

    Fast fix to support macOS
    macOS LLVM installation doesn't support OpenMP, so clang fails if
    `-fopenmp` is given. However macOS users can install LLVM or GCC
    with OpenMP support, but I would say it is rear case. This commit
    removes `-fopenmp` flags for macOS completely making it possible to
    compile non-parallel version of the package.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
+import sys
+
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
 
+extra_compile_args = []
+extra_link_args = []
+# macOS Clang doesn't support OpenMP
+if sys.platform != 'darwin':
+    extra_compile_args.append('-fopenmp')
+    extra_link_args.append('-fopenmp')
+
+
 extensions = [Extension("coniferest.calc_mean_paths",
                         ["coniferest/calc_mean_paths.pyx"],
                         include_dirs=[np.get_include()],
-                        extra_compile_args=['-fopenmp'],
-                        extra_link_args=['-fopenmp'],
+                        extra_compile_args=extra_compile_args,
+                        extra_link_args=extra_link_args,
                         )]
 
 
