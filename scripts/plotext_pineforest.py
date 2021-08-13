9c58dbb8bd75aa47dcc6e097936b49666ec5ad4b
Vladimir Korolev <balodja@gmail.com>
2021-08-13 10:31:27 +0300

    Plots for README. #9

--- scripts/plotext_pineforest.py
+++ scripts/plotext_pineforest.py
@@ -7,7 +7,7 @@ from coniferest.datasets import MalanchevDataset, Label
 from coniferest.pineforest import PineForest
 
 
-def plotme(data, anomaly_index=None):
+def plotme(data, anomaly_index=None, title=None):
     plt.clp()
     plt.colorless()
     plt.plotsize(70, 20)
@@ -15,24 +15,36 @@ def plotme(data, anomaly_index=None):
     if anomaly_index is not None:
         plt.scatter(*dataset.data[anomaly_index, :].T, marker='*', color='none')
 
+    if title is not None:
+        plt.title(title)
+
     plt.show()
 
 
-dataset = MalanchevDataset(inliers=100, outliers=10,
-                           regions=(Label.R, Label.R, Label.A))
+# Take the data
+dataset = MalanchevDataset(inliers=100, outliers=10)
+
+# Plot the data
+plotme(dataset.data, title='Plain data')
 
 
+# Plot the data with classic isolation forest anomaly detection (i.e.
+# PineForest without priors)
 pineforest = PineForest(n_subsamples=16)
 pineforest.fit(dataset.data)
 scores = pineforest.score_samples(dataset.data)
-plotme(dataset.data, np.argsort(scores)[:10])
+plotme(dataset.data, np.argsort(scores)[:10], title='PineForest without priors')
 
 
+# Add some priors
 priors = np.array([[0.0, 1.0],
                    [1.0, 1.0],
                    [1.0, 0.0]])
 
 prior_labels = np.array([Label.R, Label.R, Label.A])
+
+
+# Plot the detected anomaly data based on some known prior information
 pineforest.fit_known(dataset.data, priors, prior_labels)
 scores = pineforest.score_samples(dataset.data)
-plotme(dataset.data, np.argsort(scores)[:10])
+plotme(dataset.data, np.argsort(scores)[:10], title='PineForest with 3 priors')
