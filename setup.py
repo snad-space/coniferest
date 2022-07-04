7ea9547854f4cfbc50422ae377deb0318f02c59b
Vladimir Korolev <balodja@gmail.com>
2022-07-04 12:35:01 +0300

    Version update to 0.0.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_readme():
 
 
 setup(name='coniferest',
-      version='0.0.2',
+      version='0.0.3',
       description='Coniferous forests for better machine learning',
       long_description=get_readme(),
       long_description_content_type='text/markdown',
