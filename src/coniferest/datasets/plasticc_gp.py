snad-space/coniferest

commit 211ecc1722f9e523f44e9b6f05624497275ac9b4

Add Plasticc dataset

src/coniferest/datasets/plasticc_gp.py
@@ -1,0 +1,29 @@
+from importlib.resources import open_binary
+
+import pandas as pd
+
+from coniferest import datasets
+
+
+def plasticc_gp():
+    """
+    Load PLAsTiCC dataset of GP-approximated light curves.
+
+    It is a subset of the PLAsTiCC dataset, light curves are approximated
+    with vector Gaussian processes (Kornilov et al., 2023). Features
+    are these GP approximations and GP parameters, all normalized.
+
+    Adopted from Ishida et al. 2021:
+    https://ui.adsabs.harvard.edu/abs/2021A%26A...650A.195I/abstract
+
+    Returns
+    -------
+    data : 2-D numpy.ndarray of float32
+        2-D array of features.
+    metadata : 1-D numpy.ndarray of bool
+        1-D array of anomaly labels, True for anomalies.
+    """
+    with open_binary(datasets, "plasticc.parquet") as fh:
+        df = pd.read_parquet(fh)
+        feature_columns = [name for name in df.columns if name.startswith("feature")]
+        return df[feature_columns].to_numpy(), df["answer"].to_numpy(dtype=bool)

tests/test_datasets.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_allclose, assert_array_equal
 
-from coniferest.datasets import ztf_m31, dev_net_dataset
+from coniferest.datasets import ztf_m31, dev_net_dataset, plasticc_gp
 
 
 def test_ztf_m31():
@@ -23,3 +23,15 @@ def test_dev_net():
 
     assert metadata.shape == (data.shape[0],)
     assert_array_equal(np.unique(metadata), [-1, 1])
+
+
+def test_plasticc_gp():
+    data, metadata = plasticc_gp()
+
+    assert data.shape == (7204, 374)
+    assert data.dtype == np.float32
+    assert_allclose(data.sum(axis=1)[13], 70.540085, atol=1e-6)
+
+    assert metadata.shape == (data.shape[0],)
+    assert metadata.dtype == np.bool_
+    assert metadata.sum() == 277

MANIFEST.in
@@ -2,3 +2,4 @@ include README.md
 include src/coniferest/*.pxd
 include src/coniferest/*.pyx
 include src/coniferest/datasets/*.npz
+include src/coniferest/datasets/*.parquet

pyproject.toml
@@ -42,6 +42,8 @@ dependencies = [
 datasets = [
   # For downloading and parsing DevNet datasets
   "pandas",
+  # For Plasticc dataset
+  "pyarrow",
 ]
 dev = [
   "pytest",

src/coniferest/datasets/__init__.py
@@ -1,9 +1,10 @@
 import numpy as np
 from coniferest.label import Label
 
+from .plasticc_gp import plasticc_gp
 from .ztf_m31 import ztf_m31
 
-__all__ = ["ztf_m31", "single_outlier", "non_anomalous_outliers"]
+__all__ = ["ztf_m31", "plasticc_gp", "single_outlier", "non_anomalous_outliers"]
 
 
 class Dataset:

commit f9d8e3d7c2a7bc0772d037ca58c4a7e1d4f80c88

Convert metadata in plasticc_gp()
Fixes: 211ecc1722f9 ("Add Plasticc dataset")

src/coniferest/datasets/plasticc_gp.py
@@ -32,4 +32,8 @@ def plasticc_gp():
         except ImportError:
             raise ImportError("PyArrow is required to load PLAsTiCC datasets, install it with `pip install pyarrow` or reinstall the package with `pip install coniferest[datasets]`")
         feature_columns = [name for name in df.columns if name.startswith("feature")]
-        return df[feature_columns].to_numpy(), df["answer"].to_numpy(dtype=bool)
+        data = df[feature_columns].to_numpy()
+        # The source file uses 1 for anomaly and 0 for nominals,
+        # while we need Label.ANOMALY == -1 and Label.REGULAR == 1.
+        metadata = 1 - 2 * df["answer"].to_numpy(dtype=int)
+        return data, metadata

tests/test_datasets.py
@@ -33,5 +33,4 @@ def test_plasticc_gp():
     assert_allclose(data.sum(axis=1)[13], 70.540085, atol=1e-6)
 
     assert metadata.shape == (data.shape[0],)
-    assert metadata.dtype == np.bool_
-    assert metadata.sum() == 277
+    assert_array_equal(np.unique(metadata), [-1, 1])