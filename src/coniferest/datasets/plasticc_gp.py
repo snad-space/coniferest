90d5852f42b1d581d2a7d5d0ec252361d67fe32d
Konstantin Malanchev <hombit@gmail.com>
2024-05-16 15:10:57 -0300

    Replace local plasticc dataset with hug-face

--- src/coniferest/datasets/plasticc_gp.py
+++ src/coniferest/datasets/plasticc_gp.py
@@ -1,9 +1,13 @@
-from importlib.resources import open_binary
-
-from coniferest import datasets
+from __future__ import annotations  # "|" syntax is not available in Python 3.9
 
 
-def plasticc_gp():
+IMPORT_ERROR_MESSAGE = (
+    "`datasets' library is required to load PLAsTiCC datasets, install it with `pip install datasets` "
+    "or reinstall the package with `pip install coniferest[datasets]`"
+)
+
+
+def plasticc_gp(**kwargs):
     """
     Load PLAsTiCC dataset of GP-approximated light curves.
 
@@ -11,8 +15,16 @@ def plasticc_gp():
     with vector Gaussian processes (Kornilov et al., 2023). Features
     are these GP approximations and GP parameters, all normalized.
 
+    The dataset is hosted on Hugging Face, `datasets' package is required
+    to load it.
+
     Adopted from Ishida et al. 2021:
     https://ui.adsabs.harvard.edu/abs/2021A%26A...650A.195I/abstract
+
+    Parameters
+    ----------
+    **kwargs
+        Arbitrary keyword arguments passed to `datasets.load_dataset`.
 
     Returns
     -------
@@ -21,19 +33,17 @@ def plasticc_gp():
     metadata : 1-D numpy.ndarray of bool
         1-D array of anomaly labels, True for anomalies.
     """
-    try:
-        import pandas as pd
-    except ImportError:
-        raise ImportError("Pandas and pyarrow are required to load PLAsTiCC datasets, install them with `pip install pandas pyarrow` or reinstall the package with `pip install coniferest[datasets]`")
 
-    with open_binary(datasets, "plasticc.parquet") as fh:
-        try:
-            df = pd.read_parquet(fh)
-        except ImportError:
-            raise ImportError("PyArrow is required to load PLAsTiCC datasets, install it with `pip install pyarrow` or reinstall the package with `pip install coniferest[datasets]`")
-        feature_columns = [name for name in df.columns if name.startswith("feature")]
-        data = df[feature_columns].to_numpy()
-        # The source file uses 1 for anomaly and 0 for nominals,
-        # while we need Label.ANOMALY == -1 and Label.REGULAR == 1.
-        metadata = 1 - 2 * df["answer"].to_numpy(dtype=int)
-        return data, metadata
+    try:
+        from datasets import load_dataset
+    except ImportError:
+        raise ImportError(IMPORT_ERROR_MESSAGE)
+
+    dataset = load_dataset("snad-space/plasticc-gp", **kwargs)
+    df = dataset["train"].to_pandas()
+    features_columns = [name for name in df.columns if name.startswith("feature")]
+    data = df[features_columns].to_numpy()
+    # The source file uses 1 for anomaly and 0 for nominals,
+    # while we need Label.ANOMALY == -1 and Label.REGULAR == 1.
+    metadata = 1 - 2 * df["answer"].to_numpy(dtype=int)
+    return data, metadata
