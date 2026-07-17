a5d9b6368087987f6c66d1798e88e3ba55f071a5
Konstantin Malanchev <hombit@gmail.com>
2026-07-17 14:11:34 -0400

    Adopt ArrayRef

--- rust/src/tree/builder.rs
+++ rust/src/tree/builder.rs
@@ -3,7 +3,7 @@ use crate::tree::inner::TreeInner;
 use crate::tree::node::{Leaf, Node, SplitNode};
 use crate::utils::average_path_length;
 use itertools::Itertools;
-use ndarray::ArrayView2;
+use ndarray::ArrayRef2;
 use rand::distr::{Distribution, Uniform};
 use rand::{Rng, RngExt};
 use std::mem::MaybeUninit;
@@ -17,7 +17,7 @@ pub(crate) trait SplitAlgorithm<T> {
     /// The returned value must partition `indices` into two non-empty parts.
     fn choose_split(
         &mut self,
-        data: &ArrayView2<T>,
+        data: &ArrayRef2<T>,
         indices: &[usize],
         rng: &mut impl Rng,
     ) -> Option<(u32, T)>;
@@ -49,7 +49,7 @@ where
 {
     fn choose_split(
         &mut self,
-        data: &ArrayView2<T>,
+        data: &ArrayRef2<T>,
         indices: &[usize],
         rng: &mut impl Rng,
     ) -> Option<(u32, T)> {
@@ -93,7 +93,7 @@ where
 {
     fn choose_split(
         &mut self,
-        data: &ArrayView2<T>,
+        data: &ArrayRef2<T>,
         indices: &[usize],
         rng: &mut impl Rng,
     ) -> Option<(u32, T)> {
@@ -197,7 +197,7 @@ where
 {
     /// Build a single isolation tree from a random subsample of `data` rows.
     pub(crate) fn build(
-        data: &ArrayView2<T>,
+        data: &ArrayRef2<T>,
         n_subsamples: usize,
         max_depth: u16,
         mut rng: impl Rng,
@@ -213,7 +213,7 @@ where
     /// Build a single tree from the given subsample of `data` rows, using
     /// a custom splitting logic.
     pub(crate) fn build_with_splitter<S>(
-        data: &ArrayView2<T>,
+        data: &ArrayRef2<T>,
         mut indices: Vec<usize>,
         max_depth: u16,
         mut splitter: S,
@@ -387,7 +387,7 @@ mod tests {
     impl SplitAlgorithm<f64> for EqualHalfSplitter {
         fn choose_split(
             &mut self,
-            data: &ArrayView2<f64>,
+            data: &ArrayRef2<f64>,
             indices: &[usize],
             _rng: &mut impl Rng,
         ) -> Option<(u32, f64)> {
