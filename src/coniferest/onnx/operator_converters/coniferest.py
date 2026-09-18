985cb9b585914e4d1eb2d766819375030b5c4a8e
Jihane Vall <jihanvallsest@gmail.com>
2026-09-18 13:58:20 +0100

    ONNX: Migrate from deprecated TreeEnsembleRegressor to TreeEnsemble (#394)
    * Migrate ONNX conversion from TreeEnsembleRegressor to TreeEnsemble
    
    * Apply pre-commit fixes
    
    * Apply pre-commit fixes
    
    * Remove accidentally committed session.pickle file
    
    * Add onnx as an explicit dependency

--- src/coniferest/onnx/operator_converters/coniferest.py
+++ src/coniferest/onnx/operator_converters/coniferest.py
@@ -1,35 +1,38 @@
 def get_default_attribute_pairs():
     attrs = {}
-    attrs["post_transform"] = "NONE"
+    attrs["aggregate_function"] = 0
     attrs["n_targets"] = 1
-    attrs["nodes_treeids"] = []
-    attrs["nodes_nodeids"] = []
+    attrs["tree_roots"] = []
     attrs["nodes_featureids"] = []
     attrs["nodes_modes"] = []
-    attrs["nodes_values"] = []
+    attrs["nodes_splits"] = []
     attrs["nodes_truenodeids"] = []
+    attrs["nodes_trueleafs"] = []
     attrs["nodes_falsenodeids"] = []
-    attrs["target_treeids"] = []
-    attrs["target_nodeids"] = []
-    attrs["target_ids"] = []
-    attrs["target_weights"] = []
+    attrs["nodes_falseleafs"] = []
+    attrs["leaf_targetids"] = []
+    attrs["leaf_weights"] = []
     return attrs
 
 
-def add_node(attr_pairs, tree_id, node_id, feature_id, mode, value, true_child_id, false_child_id):
-    attr_pairs["nodes_treeids"].append(tree_id)
-    attr_pairs["nodes_nodeids"].append(node_id)
+def add_node(
+    attr_pairs,
+    feature_id,
+    threshold,
+    true_child_id,
+    true_is_leaf,
+    false_child_id,
+    false_is_leaf,
+):
     attr_pairs["nodes_featureids"].append(feature_id)
-    attr_pairs["nodes_modes"].append(mode)
-    attr_pairs["nodes_values"].append(value)
+    attr_pairs["nodes_modes"].append(0)
+    attr_pairs["nodes_splits"].append(threshold)
     attr_pairs["nodes_truenodeids"].append(true_child_id)
+    attr_pairs["nodes_trueleafs"].append(int(true_is_leaf))
     attr_pairs["nodes_falsenodeids"].append(false_child_id)
+    attr_pairs["nodes_falseleafs"].append(int(false_is_leaf))
 
 
-def add_leaf(attr_pairs, tree_id, node_id, mode, weight):
-    add_node(attr_pairs, tree_id, node_id, 0, mode, 0.0, 0, 0)
-
-    attr_pairs["target_treeids"].append(tree_id)
-    attr_pairs["target_nodeids"].append(node_id)
-    attr_pairs["target_ids"].append(0)
-    attr_pairs["target_weights"].append(weight)
+def add_leaf(attr_pairs, weight):
+    attr_pairs["leaf_targetids"].append(0)
+    attr_pairs["leaf_weights"].append(weight)
